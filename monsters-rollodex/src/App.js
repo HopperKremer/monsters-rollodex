06c07df9065ff3c8779a26b6f2124d0db0cc4419
Hopper Kremer <hopperkremer@gmail.com>
2020-12-11 11:51:11 -0500

    start app

---
+++ monsters-rollodex/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { SearchBox } from "./search-box/search-box.component";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Monster's Rollodex</h1>
+        <SearchBox />{" "}
+        <div className="card-list">
+          <div className="card-container">
+            <img
+              alt="monster"
+              src="https://robohash.org/1?set=set2&size=180x180"
+            />
+            <h2 Monster Name>
+              Monster Name
+            </h2>
+            <p>Email</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ monsters-rollodex/src/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//import from "./search-box.styles.css"
+
+export const SearchBox = () => (
+    <input
+    type="search"
+    placeholder="Search Monsters"
+    />
+)
